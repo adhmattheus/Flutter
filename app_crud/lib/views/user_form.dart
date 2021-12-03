4d97cf0b6ee21ff88801bb00d0ed3cda5be58565
adhmattheus <mattheusadhonnaysf@hotmail.com>
2021-12-03 20:31:48 -0300

    finished

--- app_crud/lib/views/user_form.dart
+++ app_crud/lib/views/user_form.dart
@@ -1,16 +1,92 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, unused_field, use_key_in_widget_constructors, avoid_print, unused_local_variable, void_checks
 
 import 'package:flutter/material.dart';
+import 'package:app_crud/models/user.dart';
+import 'package:app_crud/provider/users.dart';
+import 'package:provider/provider.dart';
 
-class UserForm extends StatelessWidget {
-  const UserForm({Key? key}) : super(key: key);
+class UserForm extends StatefulWidget {
+  @override
+  State<UserForm> createState() => _UserFormState();
+}
+
+class _UserFormState extends State<UserForm> {
+  final _form = GlobalKey<FormState>();
+
+  final Map<String, String> _formData = {};
+
+  void _loadFormData(User user) {
+    _formData['id'] = user.id;
+    _formData['name'] = user.name;
+    _formData['email'] = user.email;
+    _formData['avatarUrl'] = user.avatarUrl;
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final User user = ModalRoute.of(context)?.settings?.arguments;
+
+    _loadFormData(user);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Formulário de Usuário'),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Formulário de Usuário'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.save),
+              onPressed: () {
+                final isValid = _form.currentState!.validate();
+
+                if (isValid) {
+                  _form.currentState!.save();
+                  Provider.of<Users>(context, listen: false).put(
+                    User(
+                      id: _FormData('id'),
+                      name: '',
+                      email: '',
+                      avatarUrl: '',
+                    ),
+                  );
+                  Navigator.of(context).pop();
+                }
+              },
+            ),
+          ],
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(15),
+          child: Form(
+            key: _form,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'Nome'),
+                  validator: (value) {
+                    if (value == null || value.trim().isEmpty) {
+                      return 'Nome Inválido';
+                    }
+                    if (value.trim().length <= 3) {
+                      return 'Nome muito pequeno. No mínimo 3 letras.';
+                    }
+                    return null;
+                  },
+                  onSaved: (value) => _formData['name'] = value!,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                  onSaved: (value) => _formData['email'] = value!,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(labelText: 'URL do Avatar'),
+                  onSaved: (value) => _formData['avatarUrl'] = value!,
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
--- app_crud/lib/views/user_list.dart
+++ app_crud/lib/views/user_list.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
-import 'package:app_crud/models/user.dart';
 import 'package:app_crud/provider/users.dart';
 import 'package:app_crud/routes/app_routes.dart';
 import 'package:flutter/material.dart';
