0dadb1323b96d99e6de233b0d04dba25868a16aa
adhmattheus <mattheusadhonnaysf@hotmail.com>
2021-12-03 15:48:12 -0300

    add salve user method

--- app_crud/lib/provider/users.dart
+++ app_crud/lib/provider/users.dart
@@ -1,4 +1,6 @@
-// ignore_for_file: unused_field
+// ignore_for_file: unused_field, prefer_const_constructors, unnecessary_null_comparison
+
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:app_crud/data/dammy_users.dart';
@@ -18,4 +20,36 @@ class Users with ChangeNotifier {
   User byIndex(int i) {
     return _items.values.elementAt(i);
   }
+
+  void put(User user) {
+    if (user == null) {
+      return;
+    }
+
+    if (user.id != null &&
+        user.id.trim().isNotEmpty &&
+        _items.containsKey(user.id)) {
+      _items.update(
+        user.id,
+        (_) => User(
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          avatarUrl: user.avatarUrl,
+        ),
+      );
+    } else {
+      final id = Random().nextDouble().toString();
+      _items.putIfAbsent(
+          id,
+          () => User(
+                id: id,
+                name: user.name,
+                email: user.email,
+                avatarUrl: user.avatarUrl,
+              ));
+
+      notifyListeners();
+    }
+  } //adicionar
 }
--- app_crud/lib/views/user_list.dart
+++ app_crud/lib/views/user_list.dart
@@ -1,23 +1,25 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+import 'package:app_crud/models/user.dart';
 import 'package:app_crud/provider/users.dart';
 import 'package:flutter/material.dart';
 import 'package:app_crud/components/user_tile.dart';
 import 'package:provider/provider.dart';
 
 class UserList extends StatelessWidget {
-  const UserList({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    final Users users = Provider.of(context);
+    final Users users = Provider.of(context, listen: false);
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Lista de Usuários'), actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.add),
-          onPressed: () {},
-        ),
-      ]),
+      appBar: AppBar(
+        title: Text('Lista de Usuários'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: ListView.builder(
         itemCount: users.count,
         itemBuilder: (ctx, i) => UserTile(users.byIndex(i)),
