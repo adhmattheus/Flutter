9d825f50420c98d5a75966b2bf43cb58e5236009
adhmattheus <mattheusadhonnaysf@hotmail.com>
2021-12-03 12:43:18 -0300

    create app_crud

---
+++ app_crud/lib/data/dammy_users.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: constant_identifier_names, unnecessary_const
+
+import 'package:app_crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'mariafeijão@hotmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2021/01/17/09/11/woman-5924366_960_720.jpg',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'Rafael',
+    email: 'rafaeljogador@hotmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Fernanda',
+    email: 'fernanda_ana@hotmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2016/08/28/13/12/secondlife-1625903_960_720.jpg',
+  ),
+  '4': const User(
+    id: '4',
+    name: 'Mateus',
+    email: 'mateus.soares@hotmail.com',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2018/04/28/13/18/man-3357275_960_720.png',
+  ),
+};
