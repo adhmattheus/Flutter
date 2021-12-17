c7177a23a11357e997f7de93fa30e611dab91b96
adhmattheus <mattheusadhonnaysf@hotmail.com>
2021-12-17 11:42:37 -0300

    start application

---
+++ app_screens/lib/data/dummy_data.dart
@@ -0,0 +1,58 @@
+// ignore_for_file: constant_identifier_names, unnecessary_const
+
+import 'package:app_screens/models/category.dart';
+import '../models/category.dart';
+import 'package:flutter/material.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italiano',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Rápido e Fácil',
+    color: Colors.red,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Alemã',
+    color: Colors.indigoAccent,
+  ),
+  Category(
+    id: 'c5',
+    title: 'Leve e Saudável',
+    color: Colors.amber,
+  ),
+  Category(
+    id: 'c6',
+    title: 'Exótica',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c7',
+    title: 'Café da Manhã',
+    color: Colors.lightBlue,
+  ),
+  Category(
+    id: 'c8',
+    title: 'Asiática',
+    color: Colors.lightGreen,
+  ),
+  Category(
+    id: 'c9',
+    title: 'Francesa',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c10',
+    title: 'Verão',
+    color: Colors.teal,
+  ),
+];
